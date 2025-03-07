cf144606bab1ae52f26ef096242ed50f7e7d1879
LorealCI <loreal1806@gmail.com>
2025-03-07 15:52:04 +0000

    added code for heroku

--- zorocinematics/settings.py
+++ zorocinematics/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-uhh-c__-#)^c9q4^awawk8y^4)c_fd#tb*j2bm!258v_s8$i$r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1',]
 
 
 # Application definition
