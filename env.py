f9625b6131fb2f62f9b07a3b2e5bdb78c351cd2c
LorealCI <loreal1806@gmail.com>
2025-03-07 16:28:23 +0000

    removed env.py file

--- env.py
+++
@@ -1,4 +0,0 @@
-import os
-
-os.environ.setdefault(
-    "DATABASE_URL", "postgresql://neondb_owner:npg_GOj2LiJIwt3R@ep-crimson-thunder-a2xdjp1m.eu-central-1.aws.neon.tech/voter_limes_comma_431385")
