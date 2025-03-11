bf2b960c31c567ae814d254d3098c3707b02f747
LorealCI <loreal1806@gmail.com>
2025-03-11 16:57:34 +0000

    added to views.py

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Movie, Review
 # Create your views here.
 
 
-def my_blog(request):
-    return HttpResponse('Hello, blog!')
+def home(request):
+    items = Movie.objects.all()
+    context = {
+        'items': items
+    }
+    return render(request, context)
