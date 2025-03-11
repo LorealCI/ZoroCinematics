4177f1d8b3f442e9a6e12632a1137dc213468ff7
LorealCI <loreal1806@gmail.com>
2025-03-11 16:47:38 +0000

    added movie and review model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Movie(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(max_length=4000)
+    uploaded_on = models.DateTimeField(auto_now_add=True)
+
+
+class Review(models.Model):
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="reviewer")
+    body = models.TextField()
+    ratings = (
+        (1,1),
+        (2,2),
+        (3,3),
+        (4,4),
+        (5,5)
+    )
+    stars = models.IntegerField(choices=ratings)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.movie.title
