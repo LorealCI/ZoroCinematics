56debc87d040b3d86de70258b69c4cb0bc5b8ea6
LorealCI <loreal1806@gmail.com>
2025-03-12 15:16:31 +0000

    added copyright to my base and index.html

--- blog/views.py
+++ blog/views.py
@@ -10,3 +10,22 @@ def home(request):
         'items': items
     }
     return render(request, 'blog/index.html', context)
+
+
+def rate(request, id):
+    post = Movie.objects.get(id=id)
+    form = ReviewForm(request.POST or None)
+    if form.is_valid():
+        author = request.POST.get('author')
+        stars = request.POST.get('stars')
+        comment = request.POST.get('comment')
+        review = Review(author=author, stars=stars,  comment=comment, movie=post)
+        review.save()
+        return redirect('success')
+
+    form = ReviewForm()
+    context = {
+        "form": form
+
+    }
+    return render(request, context)
